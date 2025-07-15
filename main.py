ce0d754790459f270cd824e0716ac680e9bf6077
Alex Rong <Haowen Rong>
2025-07-15 22:19:13 +0100

    Updated main to allow for optional arguments to be used in the CLI

--- main.py
+++ main.py
@@ -1,22 +1,69 @@
-import sys
+import argparse
 from encoder import *
 from decoder import *
 
-np.set_printoptions(threshold=1000)
+argParser = argparse.ArgumentParser(
+  description='Audio Stego Tool'
+)
 
-args    = sys.argv
-numArgs = len(args)
+subparsers = argParser.add_subparsers(dest='selection', required=True)
 
-option   = args[1]
-filePath = args[2]
+# encoding args
+encode = subparsers.add_parser('encode', help='Embed a message into an audio file')
+# required args
+encode.add_argument('filePath',   type=str)
+encode.add_argument('stegoText',  type=str)
+encode.add_argument('outputPath', type=str)
+# optional args
+encode.add_argument(
+  '--startFrame', '-sf',
+  type=int,
+  default=0,
+  help='The frame the encoder will start embedding from (default: 0)'
+)
+encode.add_argument(
+  '--channels', '-ch',
+  type=int,
+  default=1,
+  help='The number of channels that will be used when embedding the message. (default: 1)'
+)
+encode.add_argument(
+  '--depth', '-d',
+  type=int,
+  default=1,
+  help='not yet implemented'
+)
 
-if option == 'encode' and numArgs == 5:
-  stegoText  = args[3]
-  outputPath = args[4]
-  encodeMessage(filePath, stegoText, outputPath, startingFrame=1)
-elif option == 'decode' and numArgs == 4:
-  messageLength = args[3]
-  decodeMessage(filePath, int(messageLength), startingFrame=1)
-else:
-  print('Invalid args')
-  sys.exit()
+# decode args
+decode = subparsers.add_parser('decode', help='Extract a message from an audio file')
+decode.add_argument('filePath',      type=str)
+decode.add_argument('messageLength', type=int)
+decode.add_argument(
+  '--startFrame', '-sf',
+  type=int,
+  default=0,
+  help='The frame the decoder will start extracting from (default: 0)'
+)
+decode.add_argument(
+  '--channels', '-ch',
+  type=int,
+  default=1,
+  help='The number of channels that will be used when extracting the message. (default: 1)'
+)
+
+args = argParser.parse_args()
+
+if args.selection == 'encode':
+  encodeMessage(
+    # required params
+    args.filePath, args.stegoText, args.outputPath,
+    # optional params
+    startingFrame=1, channels=args.channels, lsbDepth=args.depth
+    )
+elif args.selection == 'decode':
+  decodeMessage(
+    # required params
+    args.filePath, args.messageLength,
+    # optional params
+    startingFrame=1, channels=args.channels
+    )
